f1fd158ea8b3e87c22e1cd24eeb37a3eea401bf6
jinhznn <133880039+Jinhxnn@users.noreply.github.com>
2024-10-15 15:39:31 +0800

    Add validation for height and weight input to ensure positive values only.py

--- BMI.py
+++ BMI.py
@@ -17,18 +17,24 @@ weight = st.number_input("Enter your weight in KG", step = 0.1)
 height = st.number_input("Enter your height in Meters", step = 0.01)
 
 def calculate_bmi():
-    bmi = weight/(height)**2
-    bmi_thresholds = [18.5, 23, 27.5]
-    level_labels = ['Risk of nutritional deficiency','Low Risk','Moderate Risk','High Risk']
-    if bmi <= bmi_thresholds[0]:
-        level = level_labels[0]
-    elif bmi <= bmi_thresholds[1]:
-        level = level_labels[1]
-    elif bmi <= bmi_thresholds[2]:
-        level = level_labels[2]
+    if height <= 0:
+        st.error('Height needs to be more than zero. Please enter a valid number.')
+    elif weight <=0:
+        st.error('Weight needs to be more than zero. Please enter a valid number.')
     else:
-        level = level_labels[3]
-    st. success(f"Your BMI is {bmi}. You are at {level}")
+        bmi = weight/(height)**2
+        bmi_thresholds = [18.5, 23, 27.5]
+        level_labels = ['Risk of nutritional deficiency','Low Risk','Moderate Risk','High Risk']
+        
+        if bmi <= bmi_thresholds[0]:
+            level = level_labels[0]
+        elif bmi <= bmi_thresholds[1]:
+            level = level_labels[1]
+        elif bmi <= bmi_thresholds[2]:
+            level = level_labels[2]
+        else:
+            level = level_labels[3]
+        st. success(f"Your BMI is {bmi}. You are at {level}")
 
 button = st.button("Calculate BMI")
 if button:
